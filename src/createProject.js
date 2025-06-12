c3f80d54888ca8d0439e17fa6ec1f4452a83e61a
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-12 12:52:04 +0200

    Changed parameter name from user to userName
    For clarity - unique userName required, not the user's personal name.

--- src/createProject.js
+++ src/createProject.js
@@ -1,10 +1,10 @@
 // Factory function to create and return a new project object
-export default function createProject(name, user) {
+export default function createProject(name, userName) {
     const id = crypto.randomUUID();
     
     return {
         id,
         name,
-        linkedUser: user,
+        linkedUser: userName,
     }
 }
