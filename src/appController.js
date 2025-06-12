8245eb3b578e64306d9d2477ecd6bab1595a3ab6
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-12 18:58:34 +0200

    Added function to log in a user
    Checks the username and password combination and sets the user as the
    current application user if successful.

--- src/appController.js
+++ src/appController.js
@@ -7,20 +7,22 @@ import { getStoredItem, setStoredItem } from "./storageController";
 
 import {userList, projectList, taskList} from "./lists";
 
+// Currently logged in application user.  Initialized as nobody (undefined).
+let currentUser = undefined;
 
 // On initialize, pull all stored items and append (if storage is available - storageController checks for this and alerts if necessary)
 userList.push(...getStoredItem("userList"));
 projectList.push(...getStoredItem("projectList"));
 taskList.push(...getStoredItem("taskList"));
 
-function checkPassword() {
-    // 
-}
+export function logInUser(username, password) {
 
-export function logInUser() {
-    // checkUsername() && checkPassword() set currentUser = user, return logged in
-    // else return invalid username or password, please try again
-
+    const foundRegisteredUser = userList.find(registeredUser => registeredUser.username === username && registeredUser.password === password);
+    if (foundRegisteredUser) {
+        currentUser = foundRegisteredUser;
+        return "Successfully logged in";
+    }
+    else return "Invalid user or password.  Please try again";
 }
 
 export function registerNewUser(username, password) {
