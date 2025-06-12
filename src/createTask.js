dbc2a9c7f9e426ef5cc9dffaf00927c65678128f
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-12 13:09:00 +0200

    Changed parameter to receive project.id to rather receive project obj
    To avoid the incorrect part of the project being passed as an arg.
    Now the factory function will extract the necessary .id value itself.

--- src/createTask.js
+++ src/createTask.js
@@ -1,11 +1,11 @@
 // Factory function to create a new task object
-export default function createTask(projectId = "default", title, description, dueDate, priority) {
+export default function createTask(project, title, description, dueDate, priority) {
     const id = crypto.randomUUID();
     const isDone = false;
 
     return {
         id,
-        linkedProjectId: projectId,
+        linkedProjectId: project.id,
         title,
         description,
         dueDate,
