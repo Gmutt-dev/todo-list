4f2e631e1022076a7f616e7acb2350443fc2dd13
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-12 13:04:48 +0200

    Changed to accept user obj as parameter rather than user's userName
    To avoid mistakes where the factory function is called using the
    wrong argument.  Now the factory function extracts the userName
    with it's own logic.

--- src/createProject.js
+++ src/createProject.js
@@ -1,10 +1,10 @@
 // Factory function to create and return a new project object
-export default function createProject(name, userName) {
+export default function createProject(user, name) {
     const id = crypto.randomUUID();
     
     return {
         id,
         name,
-        linkedUser: userName,
+        linkedUser: user.userName
     }
 }
