6254361e6191d95752a2a836fda067f41de6b5bd
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-12 13:19:40 +0200

    Changed parameter to linkedProject for readability

--- src/createTask.js
+++ src/createTask.js
@@ -1,11 +1,11 @@
 // Factory function to create a new task object
-export default function createTask(project, title, description, dueDate, priority) {
+export default function createTask(linkedProject, title, description, dueDate, priority) {
     const id = crypto.randomUUID();
     const isDone = false;
 
     return {
         id,
-        linkedProjectId: project.id,
+        linkedProjectId: linkedProject.id,
         title,
         description,
         dueDate,
