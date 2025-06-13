55d7ae6cb9d9f8c3fdb7e9965025a7da272e3814
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-13 11:43:49 +0200

    Added retrieve all user projects on login and setting initial active projects
    Also new function added and exported to add a new project for the
    logged in user

--- src/userSession.js
+++ src/userSession.js
@@ -1,15 +1,34 @@
 import {userList, projectList, taskList} from "./lists";
+import getProjectsByUser from "./getProjectsByUser";
+import createProject from "./createProject";
+import { setStoredItem } from "./storageController";
 
 // Currently logged in application user, with projects and tasks.  Initialized as nobody (undefined).
-export let loggedInUser = undefined;
-export let activeProject = undefined;
+let loggedInUser = undefined;
+let userProjects = undefined;
+let activeProject = undefined;
 
 export function logInUser(username, password) {
     const foundRegisteredUser = userList.find(registeredUser => registeredUser.username === username && registeredUser.password === password);
     if (foundRegisteredUser) {
         loggedInUser = foundRegisteredUser;
+        userProjects = getProjectsByUser(loggedInUser);
         // TODO set active project to user's default (but what if they deleted the default project -> set to first in list?/last used?)
+        if (userProjects.find(project => project.name === "default")) {
+            activeProject = userProjects.find(project => project.name === "default");
+        }
+        else {
+            activeProject = userProjects[0];
+        }
         return "Successfully logged in";
     }
     else return "Invalid user or password.  Please try again";
+}
+
+export function addProject(name) {
+    const newProject = createProject(loggedInUser, name);
+    userProjects.push(newProject);
+    projectList.push(newProject);
+    setStoredItem("projectList", projectList);
+    //emit "newProject" event for dom update?
 }
