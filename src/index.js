49efdd5f1c02224a513e01b6262a4aa14abbb08e
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-23 14:19:06 +0200

    Removed all console testing from file.
    Temporary console tests will now be done in the domController.js file using emitter events.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
 import "./styles.css";
 
-import {
-    registerNewUser,
-    logInUser, 
-    getLoggedInUserProjects as getProjects,
-    createLoggedInUserProject as createProject} from "./appController";
-
-
-
-// TEMP testing with console
-// const newUser = createUser("user1", "Sally");
-// const newProject = createProject(newUser, "default");
-// console.log(newProject);
-// console.log(createTask(newProject,"Test Task","A task for testing","10Jan2025","high"));
-
-// Choose to either log in or create new user
-// -> New user:
-// Please enter username and password (re-enter password - try again if no match):
-console.log(registerNewUser("Sarah", "pwd"));
-console.log(registerNewUser("Pete", "pwd2"));
-// Create the user (appController) if the userName is available (appController -> storageController check), else ask to choose another username
-// Store the user (storageController(emitter?)) and confirm can now log in
-
-// -> Log in:
-// display: please enter username + password
-//  appController checks list of users and checks if username and password matches.
-// Set current user equal to this user if match.
-// Else return failed due to username or password mismatch
-
-console.log(logInUser("Sarah", "pwd"));
-console.log(getProjects());
-
-createProject("test Project");
-
-// TEMP END testing with console
+// Initialize domController with emitter event listeners
+import domController from "./domController";
+// Initialize appController with emitter event listeners
+import appController from "./appController";
