d9e2578d409023cf7d0d2d0c8466b4cabc967a8b
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-23 14:07:35 +0200

    Created domController module and imported emitter.
    Imported emitter in new domController and current userSession modules.
    
    Attempting to use only events via the emitter for communication between domController and userSession for this project.
    (Could also extend to storage controller later?)

---
+++ src/domController.js
@@ -0,0 +1,7 @@
+// Import the emitter object for DOM to userSession controller communication via events
+// NB therefore no import of userSession allowed here!
+import emitter from "./emitter";
+
+// TEMP - console testing
+
+// END TEMP
