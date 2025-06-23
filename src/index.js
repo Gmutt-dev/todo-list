ccd0e537ad31c2bff0b15701a9617db819e6899c
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-23 15:43:35 +0200

    Changed to initialize appController before domController.
    To allow for appController to be already listening for domController events, when domController is initialized.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-// Initialize domController with emitter event listeners
-import domController from "./domController";
 // Initialize appController with emitter event listeners
 import appController from "./appController";
+// Initialize domController with emitter event listeners
+import domController from "./domController";
