50399360b941e04d6e5c98856e80173665ac6dcc
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-06-24 14:27:09 +0200

    Changed to give the default project a unique id.
    To avoid issues where created tasks get attached/linked to the wrong default projects.

--- src/createProject.js
+++ src/createProject.js
@@ -1,7 +1,7 @@
 // Factory function to create and return a new project object
 export default function createProject(linkedUser, name) {
     // Make unique id "default" for the default project
-    const id = (name !== "default") ? crypto.randomUUID() : "default";
+    const id = crypto.randomUUID();
     
     return {
         id,
