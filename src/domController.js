675045534e9eaf77c267d4a146a5dabad3ce8eda
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-07-03 16:01:48 +0200

    Revised console tests after emitter event simplification.

--- src/domController.js
+++ src/domController.js
@@ -8,24 +8,24 @@ let userSession;
 // TEMP - console testing
 emitter.on("success:newUserRegistered", () => {console.log("Registration succeeded")});
 emitter.on("fail:newUserRegistered", () => {console.log("Registration failed")});
-emitter.on("success:userLoggedIn", (e) => {
+emitter.on("success:userDeregistered", () => console.log("User successfully deregistered"));
+emitter.on("fail:userDeregistered", () => console.log("Failed to deregister user"));
+emitter.on("userSessionUpdated", (e) => {
     console.log("Successfully logged in:");
     userSession = e;
     console.log(e);
 });
 emitter.on("fail:userLoggedIn", () => console.log("Failed to log in. Please try again"));
-emitter.on("success:userLoggedOut", () => {
-    console.log("Successfully logged out the user");
-    userSession = undefined;
-});
+// emitter.on("success:userLoggedOut", () => {
+//     console.log("Successfully logged out the user");
+//     userSession = undefined;
+// });
 emitter.on("fail:userLoggedOut", () => {
     console.log("Failed to log out the user");
 });
-emitter.on("success:userDeregistered", () => console.log("User successfully deregistered"));
-emitter.on("fail:userDeregistered", () => console.log("Failed to deregister user"));
-emitter.on("success:addProject", () => console.log("Added new project"));
+// emitter.on("success:addProject", () => console.log("Added new project"));
 emitter.on("fail:addProject", () => console.log("Failed to add new project"));
-emitter.on("success:addTask", () => console.log("Successfully added a new task to the active project"));
+// emitter.on("success:addTask", () => console.log("Successfully added a new task to the active project"));
 emitter.on("fail:addTask", () => console.log("Failed to add new task"));
 
 emitter.emit("request:registerNewUser", {username: "Sarah", password: "pwd1"});
