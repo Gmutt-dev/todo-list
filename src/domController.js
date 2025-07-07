0e8ecbdf1eef5b5625cfa42ea8b2ebb6bfcc60e0
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-07-07 10:48:36 +0200

    Added basic layout for DOM

--- src/domController.js
+++ src/domController.js
@@ -5,44 +5,44 @@ import "./styles.css";
 import emitter from "./emitter";
 
 
-let userSession;
-
 // TEMP - console testing
-emitter.on("success:newUserRegistered", () => {console.log("Registration succeeded")});
-emitter.on("fail:newUserRegistered", () => {console.log("Registration failed")});
-emitter.on("success:userDeregistered", () => console.log("User successfully deregistered"));
-emitter.on("fail:userDeregistered", () => console.log("Failed to deregister user"));
-emitter.on("userSessionUpdated", (e) => {
-    console.log("Successfully logged in:");
-    userSession = e;
-    console.log(e);
-});
-emitter.on("fail:userLoggedIn", () => console.log("Failed to log in. Please try again"));
-// emitter.on("success:userLoggedOut", () => {
-//     console.log("Successfully logged out the user");
-//     userSession = undefined;
+// let userSession;
+
+// emitter.on("success:newUserRegistered", () => {console.log("Registration succeeded")});
+// emitter.on("fail:newUserRegistered", () => {console.log("Registration failed")});
+// emitter.on("success:userDeregistered", () => console.log("User successfully deregistered"));
+// emitter.on("fail:userDeregistered", () => console.log("Failed to deregister user"));
+// emitter.on("userSessionUpdated", (e) => {
+//     console.log("Successfully logged in:");
+//     userSession = e;
+//     console.log(e);
 // });
-emitter.on("fail:userLoggedOut", () => {
-    console.log("Failed to log out the user");
-});
-// emitter.on("success:addProject", () => console.log("Added new project"));
-emitter.on("fail:addProject", () => console.log("Failed to add new project"));
-// emitter.on("success:addTask", () => console.log("Successfully added a new task to the active project"));
-emitter.on("fail:addTask", () => console.log("Failed to add new task"));
+// emitter.on("fail:userLoggedIn", () => console.log("Failed to log in. Please try again"));
+// // emitter.on("success:userLoggedOut", () => {
+// //     console.log("Successfully logged out the user");
+// //     userSession = undefined;
+// // });
+// emitter.on("fail:userLoggedOut", () => {
+//     console.log("Failed to log out the user");
+// });
+// // emitter.on("success:addProject", () => console.log("Added new project"));
+// emitter.on("fail:addProject", () => console.log("Failed to add new project"));
+// // emitter.on("success:addTask", () => console.log("Successfully added a new task to the active project"));
+// emitter.on("fail:addTask", () => console.log("Failed to add new task"));
 
-emitter.emit("request:registerNewUser", {username: "Sarah", password: "pwd1"});
-emitter.emit("request:registerNewUser", {username: "Peter", password: "pwd2"});
-emitter.emit("request:registerNewUser", {username: "John", password: "pwd3"});
+// emitter.emit("request:registerNewUser", {username: "Sarah", password: "pwd1"});
+// emitter.emit("request:registerNewUser", {username: "Peter", password: "pwd2"});
+// emitter.emit("request:registerNewUser", {username: "John", password: "pwd3"});
 
-emitter.emit("request:logInUser", {username: "Sarah", password: "pwd1"});
+// emitter.emit("request:logInUser", {username: "Sarah", password: "pwd1"});
 
-// emitter.emit("request:logOutUser", {});
+// // emitter.emit("request:logOutUser", {});
 
-emitter.emit("request:deregisterUser", {username: "Peter", password: "pwd2"})
+// emitter.emit("request:deregisterUser", {username: "Peter", password: "pwd2"})
 
-emitter.emit("request:addProject", {projectName: "Garden"});
+// emitter.emit("request:addProject", {projectName: "Garden"});
 
-emitter.emit("request:addTask", {project: userSession.userProjects[0], title: "Test title", description: "Test description", dueDate: new Date(2025, 7, 9) , priority:"high"});
+// emitter.emit("request:addTask", {project: userSession.userProjects[0], title: "Test title", description: "Test description", dueDate: new Date(2025, 7, 9) , priority:"high"});
 
 // emitter.emit("request:updateTask", {id: "f3379445-f0f5-4933-a50e-566fb976e8a6", description: "Updated test description"});
 
