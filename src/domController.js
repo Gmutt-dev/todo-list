254d9a9b7b67d2ca91f66eabd589d2c69c67af27
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-07-08 17:59:19 +0200

    Added modal for log in.

--- src/domController.js
+++ src/domController.js
@@ -17,7 +17,7 @@ function drawDefaultApp() {
         
         const logoDiv = document.createElement("div");
         logoDiv.classList.add("logo");
-        const logoText = document.createElement("H1");
+        const logoText = document.createElement("h1");
         logoText.textContent = "Just DO it!";
         logoDiv.appendChild(logoText);
         const logoSubText = document.createElement("p");
@@ -29,7 +29,84 @@ function drawDefaultApp() {
         loginButton.type = "button";
         loginButton.classList.add("login-button");
         loginButton.textContent = "Log in";
+        loginButton.addEventListener("click", loginButtonHandler)
         header.appendChild(loginButton);
+
+        function loginButtonHandler(e) {
+            const modal = document.querySelector(".modal-container");
+
+            modal.classList.add("login-form");
+            
+            const title = document.createElement("h2");
+            title.textContent = "Log in request form";
+            modal.appendChild(title);
+
+            const para1 = document.createElement("p");
+            para1.textContent = "Please enter your username and password and click submit.";
+            modal.appendChild(para1);
+
+            const warningPara = document.createElement("p");
+            warningPara.textContent = "IMPORTANT! - passwords and user content is not protected/encrypted.  However, all data remains ONLY on your device.  Please don't include any sensitive information anywhere in the app."
+            modal.appendChild(warningPara);
+
+            const form = document.createElement("form");
+            form.method = "dialog";
+            
+            const usernameLabel = document.createElement("label");
+            usernameLabel.for = "username-input";
+            usernameLabel.textContent = "Username:"
+            form.appendChild(usernameLabel);
+
+            const usernameInput = document.createElement("input");
+            usernameInput.type = "text";
+            usernameInput.id = "username-input";
+            usernameInput.required = "true";
+            form.appendChild(usernameInput);
+
+            const passwordLabel = document.createElement("label");
+            passwordLabel.for = "password-input";
+            passwordLabel.textContent = "Password:";
+            form.appendChild(passwordLabel);
+
+            const passwordInput = document.createElement("input");
+            passwordInput.type = "password";
+            passwordInput.id = "password-input";
+            passwordInput.required = "true";
+            form.appendChild(passwordInput);
+            
+            const submitButton = document.createElement("button");
+            submitButton.type = "button";
+            submitButton.textContent = "Submit";
+            submitButton.addEventListener("click", (e) => {
+                emitter.emit("request:logInUser", {
+                    username: usernameInput.value,
+                    password: passwordInput.value
+                })
+                modal.classList.remove("login-form");
+                modal.close();
+                modal.textContent = "";
+            })
+            form.appendChild(submitButton);
+            
+            const resetButton = document.createElement("button");
+            resetButton.type = "reset";
+            resetButton.textContent = "Reset";
+            form.appendChild(resetButton);
+            
+            const cancelButton = document.createElement("button");
+            cancelButton.type = "button";
+            cancelButton.textContent = "Cancel";
+            cancelButton.addEventListener("click", (e) => {
+                modal.close();
+                modal.classList.remove("login-form");
+                modal.textContent = "";
+            })
+            form.appendChild(cancelButton);
+            
+            modal.appendChild(form);
+
+            modal.showModal();
+        }
     
         const registerButton = document.createElement("button");
         registerButton.type = "button";
@@ -86,7 +163,7 @@ initializeAppGrid();
 // emitter.on("fail:userDeregistered", () => console.log("Failed to deregister user"));
 // emitter.on("userSessionUpdated", (e) => {
 //     console.log("Successfully logged in:");
-//     userSession = e;
+//     const userSession = e;
 //     console.log(e);
 // });
 // emitter.on("fail:userLoggedIn", () => console.log("Failed to log in. Please try again"));
