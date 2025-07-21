b4c356ad36b66484546a8cf8186ab763ee15b4ec
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-07-21 11:27:10 +0200

    Added functionality to register a new user

---
+++ src/DOM-handlers.js
@@ -0,0 +1,194 @@
+import { createHTMLElement } from "./DOM-fns";
+import emitter from "./emitter";
+
+export function loginButtonHandler(e) {
+    const modal = document.querySelector(".modal-container");
+
+    modal.classList.add("login-form");
+    
+    modal.appendChild(
+        createHTMLElement("h2", {
+            textContent: "Log in request form"
+        })
+    );
+
+    modal.appendChild(
+        createHTMLElement("p", {
+            textContent: "Please enter your username and password and click submit."
+        })
+    );
+
+    modal.appendChild(
+        createHTMLElement("p", {
+            textContent: "IMPORTANT! - passwords and user content is not protected/encrypted.  However, all data remains ONLY on your device.  Please don't include any sensitive information anywhere in the app."
+        })
+    );
+
+    const form = createHTMLElement("form", {
+        method: "dialog"
+    })
+    
+    form.appendChild(
+        createHTMLElement("label", {
+            htmlFor: "username-input",
+            textContent: "Username"
+        })
+    );
+
+    const usernameInput = createHTMLElement("input", {
+        type: "text:",
+        id: "username-input",
+        required: true
+    })
+    form.appendChild(usernameInput);
+
+    form.appendChild(
+        createHTMLElement("label", {
+            htmlFor: "password-input",
+            textContent: "Password"
+        })
+    );
+
+    const passwordInput = createHTMLElement("input", {
+        type: "password",
+        id: "password-input",
+        required: true
+    })
+    form.appendChild(passwordInput);
+    
+    const submitButton = createHTMLElement("button", {
+        type: "button",
+        textContent: "Submit"
+    })
+    submitButton.addEventListener("click", (e) => {
+        emitter.emit("request:logInUser", {
+            username: usernameInput.value,
+            password: passwordInput.value
+        })
+        modal.classList.remove("login-form");
+        modal.close();
+        modal.textContent = "";
+    })
+    form.appendChild(submitButton);
+    
+    const resetButton = createHTMLElement("button", {
+        type: "reset",
+        textContent: "Reset"
+    })
+    form.appendChild(resetButton);
+    
+    const cancelButton = createHTMLElement("button", {
+        type: "button",
+        textContent: "Cancel"
+    });
+    cancelButton.addEventListener("click", (e) => {
+        modal.close();
+        modal.classList.remove("login-form");
+        modal.textContent = "";
+    })
+
+    form.appendChild(cancelButton);
+    
+    modal.appendChild(form);
+
+    modal.showModal();
+}
+
+export function registerButtonHandler(e) {
+    const modal = document.querySelector(".modal-container");
+
+    modal.classList.add("register-form");
+    
+    modal.appendChild(
+        createHTMLElement("h2", {
+            textContent: "Registration request form"
+        })
+    );
+
+    modal.appendChild(
+        createHTMLElement("p", {
+            textContent: "Please enter your requested username and password and click submit."
+        })
+    );
+
+    modal.appendChild(
+        createHTMLElement("p", {
+            textContent: "IMPORTANT! - passwords and user content is not protected/encrypted.  However, all data remains ONLY on your device.  Please don't include any sensitive information anywhere in the app."
+        })
+    );
+
+    const form = createHTMLElement("form", {
+        method: "dialog"
+    })
+    
+    form.appendChild(
+        createHTMLElement("label", {
+            htmlFor: "username-input",
+            textContent: "Username"
+        })
+    );
+
+    const usernameInput = createHTMLElement("input", {
+        type: "text:",
+        id: "username-input",
+        required: true
+    })
+    form.appendChild(usernameInput);
+
+    form.appendChild(
+        createHTMLElement("label", {
+            htmlFor: "password-input",
+            textContent: "Password"
+        })
+    );
+
+    const passwordInput = createHTMLElement("input", {
+        type: "password",
+        id: "password-input",
+        required: true
+    })
+    
+    form.appendChild(passwordInput);
+    
+    const submitButton = createHTMLElement("button", {
+        type: "button",
+        textContent: "Submit"
+    })
+    submitButton.addEventListener("click", (e) => {
+        emitter.emit("request:registerNewUser", {
+            username: usernameInput.value,
+            password: passwordInput.value
+        })
+        modal.classList.remove("register-form");
+        modal.close();
+        modal.textContent = "";
+    })
+    form.appendChild(submitButton);
+    
+    const resetButton = createHTMLElement("button", {
+        type: "reset",
+        textContent: "Reset"
+    })
+    form.appendChild(resetButton);
+    
+    const cancelButton = createHTMLElement("button", {
+        type: "button",
+        textContent: "Cancel"
+    });
+    cancelButton.addEventListener("click", (e) => {
+        modal.close();
+        modal.classList.remove("register-form");
+        modal.textContent = "";
+    })
+
+    form.appendChild(cancelButton);
+    
+    modal.appendChild(form);
+
+    modal.showModal();
+}
+
+export function logoutButtonHandler(e) {
+    userSessionClone = undefined;
+    drawDefaultApp();
+}
--- src/domController.js
+++ src/domController.js
@@ -4,6 +4,7 @@ import "./styles.css";
 // NB therefore no import of appController allowed here!  Must use emitter!
 import emitter from "./emitter";
 import { createHTMLElement } from "./DOM-fns";
+import { loginButtonHandler, registerButtonHandler, logoutButtonHandler } from "./DOM-handlers";
 
 const appContainer = document.querySelector(".app-container");
 let userSessionClone = undefined;
@@ -38,104 +39,14 @@ function drawDefaultApp() {
         loginButton.addEventListener("click", loginButtonHandler)
         header.appendChild(loginButton);
 
-        function loginButtonHandler(e) {
-            const modal = document.querySelector(".modal-container");
 
-            modal.classList.add("login-form");
-            
-            modal.appendChild(
-                createHTMLElement("h2", {
-                    textContent: "Log in request form"
-                })
-            );
-
-            modal.appendChild(
-                createHTMLElement("p", {
-                    textContent: "Please enter your username and password and click submit."
-                })
-            );
-
-            modal.appendChild(
-                createHTMLElement("p", {
-                    textContent: "IMPORTANT! - passwords and user content is not protected/encrypted.  However, all data remains ONLY on your device.  Please don't include any sensitive information anywhere in the app."
-                })
-            );
-
-            const form = createHTMLElement("form", {
-                method: "dialog"
-            })
-            
-            form.appendChild(
-                createHTMLElement("label", {
-                    htmlFor: "username-input",
-                    textContent: "Username"
-                })
-            );
-
-            const usernameInput = createHTMLElement("input", {
-                type: "text:",
-                id: "username-input",
-                required: true
-            })
-            form.appendChild(usernameInput);
-
-            form.appendChild(
-                createHTMLElement("label", {
-                    htmlFor: "password-input",
-                    textContent: "Password"
-                })
-            );
-
-            const passwordInput = createHTMLElement("input", {
-                type: "password",
-                id: "password-input",
-                required: true
-            })
-            form.appendChild(passwordInput);
-            
-            const submitButton = createHTMLElement("button", {
-                type: "button",
-                textContent: "Submit"
-            })
-            submitButton.addEventListener("click", (e) => {
-                emitter.emit("request:logInUser", {
-                    username: usernameInput.value,
-                    password: passwordInput.value
-                })
-                modal.classList.remove("login-form");
-                modal.close();
-                modal.textContent = "";
-            })
-            form.appendChild(submitButton);
-            
-            const resetButton = createHTMLElement("button", {
-                type: "reset",
-                textContent: "Reset"
-            })
-            form.appendChild(resetButton);
-            
-            const cancelButton = createHTMLElement("button", {
-                type: "button",
-                textContent: "Cancel"
-            });
-            cancelButton.addEventListener("click", (e) => {
-                modal.close();
-                modal.classList.remove("login-form");
-                modal.textContent = "";
-            })
-
-            form.appendChild(cancelButton);
-            
-            modal.appendChild(form);
-
-            modal.showModal();
-        }
     
         const registerButton = createHTMLElement("button", {
             type: "button",
             className: "register-button",
             textContent: "Register"
         })
+        registerButton.addEventListener("click", registerButtonHandler);
         header.appendChild(registerButton);
     }
 
@@ -230,10 +141,7 @@ emitter.on("userSessionUpdated", (e) => {
             logoutButton.addEventListener("click", logoutButtonHandler);
             userDetailDiv.appendChild(logoutButton);
 
-            function logoutButtonHandler(e) {
-                userSessionClone = undefined;
-                drawDefaultApp();
-            }
+
                 
             header.appendChild(userDetailDiv);
         }
